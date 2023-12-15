cfccd2b0acaa796fdfb3b06eb4de77866211a5e9
fany <alvarezpestefania@outlook.com>
2023-12-14 22:36:51 -0600

    create token with jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,21 +1,37 @@
 import { response } from "express";
 import User from "../models/user.models.js";
+import bcrypt from "bcryptjs";
+import { createAccesToken } from '../libs/jwt.js'
+
 
 //procesar peticiones
 export const register = async (req, res) => {
     const { username, email, password } = (req.body)
     // console.log(username, email, password )
     try {
+
+        const passwordHash = await bcrypt.hash(password, 10)
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash
         })//solo esta en el backend 
+
         const userSaved = await newUser.save()
-        res.json(userSaved)
+        const token = await createAccesToken({ id: userSaved._id });
+
+        res.cookie('token cookie', token)
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createAt: userSaved.createdAt,
+            upddateAt: userSaved.updatedAt
+        })
 
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: error.message })
     }
 };
 
