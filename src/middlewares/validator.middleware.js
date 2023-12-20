29804d9ef154aa0012ad4a4e05a71320c5cb4ef1
fany <alvarezpestefania@outlook.com>
2023-12-19 22:02:22 -0600

    add part of validations in tasks and users

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,13 @@
+import { Schema } from "zod"
+
+//
+export const validateSchema = (schema) => (req, resp, next) => {
+    //los schemas de zod tienne un metodo que hace la validacion
+    try {
+        schema.parse(req.body)
+        next()
+    } catch (error) {
+        console.log(error.errors)
+        return resp.status(400).json({ error: error.errors.map((error) => error.message) })
+    }
+}
