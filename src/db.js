155bbc998121fb2edbf82c896e8f4ad0504237b5
fany <alvarezpestefania@outlook.com>
2024-01-10 22:22:34 -0600

    add changes on conection with db mongose

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 //conexion de mongo
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://alvarezpestefania:project-tasks@cluster0.dulk6if.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb+srv://alvarezpestefania:ZvECDSHSCNw98Rt9@cluster0.yry6cqu.mongodb.net/?retryWrites=true&w=majority')
         console.log('>>>DB CONNECT')
 
     } catch (error) {
