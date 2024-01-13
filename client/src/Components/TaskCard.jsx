f0ca6d56fba3d680054da3a5185662eabc04b535
fany <alvarezpestefania@outlook.com>
2024-01-12 22:15:54 -0600

    add delete task

--- client/src/Components/TaskCard.jsx
+++ client/src/Components/TaskCard.jsx
@@ -1,9 +1,7 @@
 import { useTasks } from "../context/TaskContext.jsx";
-import { Link } from "react-router-dom";
 
 function TaskCard({ task }) {
   const { deleteTask } = useTasks();
-
   return (
     <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
       <h3 className="text-2xl font-bold mb-2">{task.title}</h3>
@@ -11,8 +9,8 @@ function TaskCard({ task }) {
       <p className="text-stale-300 text-xl">{task.description}</p>
       <p>{new Date(task.date).toLocaleDateString()}</p>
       <div className="my-4 flex gap-x-4 justify-end px-2">
-        <button onClick={() => deleteTask(task._id)}>Delete</button>
-        <Link to={`/tasks/${task._id}`}>Edit</Link>
+      <button onClick={() => deleteTask(task._id)}>Delete</button>
+        <button>Edit</button>
       </div>
     </div>
   );
